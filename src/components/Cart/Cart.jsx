055937b0084bd5d7331113d7ca53c239bc3e1711
amirayhan <jaber.ac1983@gmail.com>
2023-08-29 16:41:15 +0600

    add a paragraph in cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -21,6 +21,11 @@ const Cart = ({ cart }) => {
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi dolor maiores sint architecto, non rerum, neque ducimus eius explicabo quibusdam omnis facilis! Porro in velit ullam numquam facilis impedit, nihil cumque ab natus dignissimos voluptate iste quod quo ut distinctio vero? Veniam vel placeat alias dolores iusto consectetur, doloremque obcaecati earum vero atque, est delectus facere nobis quam aut error ut quaerat? Vero inventore velit rem alias nemo quasi, earum
+                magnam laborum fuga dolores hic dolore est quam ex nihil suscipit reprehenderit. Cumque architecto at vero obcaecati vitae voluptatibus illo labore laborum totam nisi excepturi quas veniam delectus, odit, dolores dolor! Quia nostrum ex qui! Dolore fugit, rerum nemo praesentium cumque officia repudiandae quas earum quis, quo aperiam. Quidem, aut corporis neque dolorum praesentium repudiandae saepe, quod in, quas blanditiis ullam porro ducimus cupiditate quaerat recusandae? Impedit
+                necessitatibus in tempore assumenda velit autem perferendis inventore odio. Facere libero fugiat officiis.
+            </p>
             <span>
                 <b>Grand Total: ${grandTotal.toFixed(2)}</b>
             </span>
