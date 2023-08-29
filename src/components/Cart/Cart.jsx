560bf24e02192de725a6e5290f7d6c09a9f5c3f4
amirayhan <jaber.ac1983@gmail.com>
2023-08-29 16:40:08 +0600

    Make Cart component and complete all calculation and show in Cart Final

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,21 +4,20 @@ import "./Cart.css";
 const Cart = ({ cart }) => {
     let totalPrice = 0;
     let totalShipping = 0;
-    let quantity = 0;
+
     for (const product of cart) {
         // if (product.quantity === 0) {
         //     product.quantity = 1;
         // }
-        totalPrice = totalPrice + product.price * product.quantity;
-        totalShipping = totalShipping + product.shipping * product.quantity;
-        quantity = quantity + product.quantity * product.quantity;
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
     }
     const tax = (totalPrice * 7) / 100;
     const grandTotal = totalPrice + totalShipping + tax;
     return (
         <div className="summary">
             <h4>Order Summary</h4>
-            <p>Selected Items: {quantity}</p>
+            <p>Selected Items: {cart.length}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { getShoppingCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 
@@ -12,34 +11,9 @@ const Shop = () => {
             .then((data) => setProducts(data));
     }, []);
 
-    useEffect(() => {
-        const storedCart = getShoppingCart();
-        const savedCart = [];
-        for (const id in storedCart) {
-            const addedProduct = products.find((product) => product.id === id);
-            if (addedProduct) {
-                const quantity = storedCart[id];
-                addedProduct.quantity = quantity;
-                savedCart.push(addedProduct);
-            }
-        }
-        setCart(savedCart);
-    }, [products]);
-
     const handleAddToCart = (product) => {
-        let newCart = [];
-        const exists = cart.find((pd) => pd.id === product.id);
-        if (!exists) {
-            product.quantity = 1;
-            newCart = [...cart, product];
-        } else {
-            exists.quantity = exists.quantity + 1;
-            const remaining = cart.filter((pd) => pd.id !== product.id);
-            newCart = [...remaining, exists];
-        }
-        // const newCart = [...cart, product];
+        const newCart = [...cart, product];
         setCart(newCart);
-        addToDb(product.id);
     };
     return (
         <div className="shop mt-4">
