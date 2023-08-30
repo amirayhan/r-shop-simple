04abfeb2280b2d20c34c5b6c69a226f1b6039aa9
amirayhan <jaber.ac1983@gmail.com>
2023-08-30 17:31:02 +0600

    add all calculation in cart from  localStorage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,20 +4,18 @@ import "./Cart.css";
 const Cart = ({ cart }) => {
     let totalPrice = 0;
     let totalShipping = 0;
-
+    let quantity = 0;
     for (const product of cart) {
-        // if (product.quantity === 0) {
-        //     product.quantity = 1;
-        // }
-        totalPrice = totalPrice + product.price;
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = (totalPrice * 7) / 100;
     const grandTotal = totalPrice + totalShipping + tax;
     return (
         <div className="summary">
             <h4>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
