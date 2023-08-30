7be8f767b0a81b2ea07038e9a3ad613cb08d2b03
amirayhan <jaber.ac1983@gmail.com>
2023-08-30 18:05:37 +0600

    install react router and ready header Link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 import logo from "../../images/Logo.svg";
 import "./Header.css";
 const Header = () => {
@@ -14,10 +15,18 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="/order">Order</Nav.Link>
-                        <Nav.Link href="/order-review">Order Review</Nav.Link>
-                        <Nav.Link href="/manage-Inventory">Manage Inventory</Nav.Link>
-                        <Nav.Link href="/login">Login</Nav.Link>
+                        <Link className="nav-link" to="/order">
+                            Order
+                        </Link>
+                        <Link className="nav-link" to="/order-review">
+                            Order Review
+                        </Link>
+                        <Link className="nav-link" to="/manage-Inventory">
+                            Manage Inventory
+                        </Link>
+                        <Link className="nav-link" to="/login">
+                            Login
+                        </Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,45 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Layout/Home.jsx";
+import Login from "./components/Login/Login.jsx";
+import ManageInventory from "./components/ManageInventory/ManageInventory.jsx";
+import Order from "./components/Order/Order.jsx";
+import OrderReview from "./components/OrderReview/OrderReview.jsx";
+import Shop from "./components/Shop/Shop.jsx";
 import "./index.css";
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home></Home>,
+        children: [
+            {
+                path: "/",
+                element: <Shop></Shop>,
+            },
+            {
+                path: "order",
+                element: <Order></Order>,
+            },
+            {
+                path: "order-review",
+                element: <OrderReview></OrderReview>,
+            },
+            {
+                path: "manage-inventory",
+                element: <ManageInventory></ManageInventory>,
+            },
+            {
+                path: "login",
+                element: <Login></Login>,
+            },
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
