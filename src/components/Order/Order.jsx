6c3365942e96a2e56472fbecb5d91d9ff3b8ce98
amirayhan <jaber.ac1983@gmail.com>
2023-08-31 17:44:46 +0600

    Remove Item handler and Make Data consistent across UI

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { removeFromDb } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Order = () => {
-    const cart = useLoaderData();
+    const savedCart = useLoaderData();
+    const [cart, setCart] = useState(savedCart);
+
+    const handleRemveFromCart = (id) => {
+        const remaining = cart.filter((product) => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    };
     return (
         <div className="shop mt-4">
             <div className="container">
                 <div className="row">
                     <div className="col-md-9">
                         <div className="products">
-                            <div className="row">
-                                <h1>Order Details: {cart.length}</h1>
-                            </div>
+                            {cart.map((product) => (
+                                <ReviewItem key={product.id} product={product} handleRemveFromCart={handleRemveFromCart}></ReviewItem>
+                            ))}
                         </div>
                     </div>
                     <div className="col-md-3">
---
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,30 @@
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import React from "react";
+import "./ReviewItem.css";
+
+const ReviewItem = ({ product, handleRemveFromCart }) => {
+    const { id, img, name, price, quantity } = product;
+    return (
+        <div className="review_item p-2 d-flex justify-content-between">
+            <div className="product_content d-flex">
+                <img src={img} alt="" />
+                <div className="content">
+                    <h3>{name}</h3>
+                    <p>
+                        Price: <span className="text-warning">${price}</span>
+                    </p>
+                    <p>
+                        Quantity: <span className="text-warning">{quantity} Pcs</span>
+                    </p>
+                </div>
+            </div>
+            <button onClick={()=>handleRemveFromCart(id)} className="delete_button">
+                <FontAwesomeIcon icon={faTrash} />
+            </button>
+        </div>
+    );
+};
+
+export default ReviewItem;
